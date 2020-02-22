9bf260d63eba81626938c64a959195fc604d2fa8
21Hvrry <60355625+21Hvrry@users.noreply.github.com>
2020-02-22 10:50:05 -0800

    Add files via upload

---
+++ self-portrait/sketch.js
@@ -0,0 +1,72 @@
+let eyespacing = 35;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(50,50,200);
+    // fill(255,0,0);
+  // ellipse(width/2, height/2-50, 160,200);
+  fill(100,100,60);
+  console.log("x: " + mouseX + ", y: " + mouseY);
+
+  fill (200,140,100);
+  ellipse(width/2, height/2, 160,200);
+  
+  //hair
+  push();
+  fill(0);
+  //noFill();
+  rotate(0);
+  arc(width/2,145,140,90,PI,0);
+  pop();
+
+  //eyes
+  push();
+    fill(255);
+    translate(0,175);
+      push();
+        translate(width/2-eyespacing,0);
+        ellipse(0,0,35,25);
+          push();
+            fill(140,90,0);
+            ellipse(0,0,17,17);
+              push();
+                fill(0);
+                ellipse(0,0,7,7);
+              pop();
+          pop();
+      pop();
+  pop();
+    push();
+      fill(255);
+      translate(0,175);
+        push();
+          translate(width/2+eyespacing,0);
+          ellipse(0,0,35,25);
+            push();
+              fill(140,90,0);
+              ellipse(0,0,17,17);
+                push();
+                  fill(0);
+                  ellipse(0,0,7,7);
+                pop();
+            pop();
+        pop();
+    pop();
+  
+  //mouth
+  push();
+  fill(300);
+  arc(width/2,230,85,90,0,PI);
+  pop();
+  
+  //beginShape();
+  // vertex(120,177);
+  // vertex(270,177);
+  // vertex(width/2,99)
+  // endShape(CLOSE);
+  
+  
+}
